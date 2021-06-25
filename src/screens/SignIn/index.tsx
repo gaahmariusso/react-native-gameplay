c64e041eedbc1be11fe5957377aacbb1b08c4cf4
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2021-06-25 00:44:35 -0300

    interface home / detalhes servidor / navigation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,48 +1,50 @@
 import React from 'react';
-import { 
-    View, 
-    Text, 
+import {
+    View,
+    Text,
     Image,
-    StatusBar,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
-export function SignIn() {
-    return(
-        <View style={styles.container}>
-            <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
+import { ButtonIcon } from '../../components/ButtonIcon';
 
-            <Image 
-            source={IllustrationImg}
-            style={styles.image}
-            resizeMode="stretch"
+export function SignIn() {
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
             />
 
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {`\n`}
-                    suas jogatinas {`\n`}
-                    facilmente
+                    Conecte-se {'\n'}
+                    e organize suas {'\n'}
+                    jogatinas
                 </Text>
 
                 <Text style={styles.subtitle}>
-                    Crie grupos para jogar seus games {`\n`}
+                    Crie grupos para jogar seus games {'\n'}
                     favoritos com seus amigos
                 </Text>
 
-                <ButtonIcon 
+                <ButtonIcon
                     title="Entrar com Discord"
                     activeOpacity={0.7}
+                    onPress={handleSignIn}
                 />
 
             </View>
         </View>
-  );
+    );
 }
